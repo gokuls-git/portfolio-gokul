bea5c28f749a8f12a57f9d9e8db837543526385c
Gokul S <gokuls@Gokuls-MacBook-Air.local>
2025-03-03 22:56:38 +0530

    minor-changes-ui

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,10 +57,10 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Gokul, a Next.js Developer based in Croatia.
+            Hi! I&apos;m Gokul, a Frontend Developer (UI) based in India.
           </p>
 
-          <a href="#about">
+          <a href="#projects">
             <MagicButton
               title="Show my work"
               icon={<FaLocationArrow />}
--- next.config.mjs
+++ next.config.mjs
@@ -1,40 +1,33 @@
-import {withSentryConfig} from '@sentry/nextjs';
+import { withSentryConfig } from "@sentry/nextjs";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withSentryConfig(nextConfig, {
-// For all available options, see:
-// https://github.com/getsentry/sentry-webpack-plugin#options
+export default withSentryConfig(
+  nextConfig,
+  {
+    // Suppresses source map uploading logs during build
+    silent: true,
+    org: "gokul-ui-dev", // Update with your Sentry organization
+    project: "nextjs-ui", // Update with your Sentry project
+  },
+  {
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
 
-// Suppresses source map uploading logs during build
-silent: true,
-org: "javascript-mastery",
-project: "javascript-nextjs",
-}, {
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: true,
 
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
+    // Uncomment if you want to route requests through a custom endpoint (check middleware compatibility)
+    // tunnelRoute: "/sentry-monitor",
 
-// Transpiles SDK to be compatible with IE11 (increases bundle size)
-transpileClientSDK: true,
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
 
-// Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-// tunnelRoute: "/monitoring",
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
 
-// Hides source maps from generated client bundles
-hideSourceMaps: true,
-
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
-
-// Enables automatic instrumentation of Vercel Cron Monitors.
-// See the following for more information:
-// https://docs.sentry.io/product/crons/
-// https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
-});
+    // Enables automatic instrumentation of Vercel Cron Monitors (if using Vercel)
+    automaticVercelMonitors: true,
+  }
+);
